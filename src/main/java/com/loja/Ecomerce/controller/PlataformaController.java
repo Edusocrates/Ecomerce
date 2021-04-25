0c1243e0259b86b04a3a78faaf7dae555e92729e
Eduardo Socrates <edu5390@gmail.com>
2021-04-25 20:53:55 -0300

    creating the controller for each objects

---
+++ src/main/java/com/loja/Ecomerce/controller/PlataformaController.java
@@ -0,0 +1,9 @@
+package com.loja.Ecomerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("plataforma")
+public class PlataformaController {
+}
